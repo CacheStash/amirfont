6483c37f120ec4bbcfa116644cf4556cef813e17
CacheStash <donationthing@gmail.com>
2026-02-25 15:13:52 +0700

    update 2026-02-25_15-13-13: fix spa routing and add image caching

--- pages/shop/Checkout.tsx
+++ pages/shop/Checkout.tsx
@@ -473,6 +473,9 @@ if (subscribe) {
                       alert("PAYPAL_GATEWAY_ERROR: COULD NOT INITIALIZE TRANSACTION. CHECK YOUR EMAIL FORMAT OR PAYMENT METHOD.");
                     }}
                     />
+                    <button className="w-full mt-4 bg-red-600 text-white py-4 font-black text-sm tracking-[0.2em] border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] active:shadow-none active:translate-x-[2px] active:translate-y-[2px] transition-all">
+                      TEST_BUTTON_FULL_BLOCK_VALIDATION
+                    </button>
                   </div>
                 </div>
               </div>
