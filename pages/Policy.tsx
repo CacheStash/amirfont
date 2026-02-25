603c47e137863b37bdb12e754c9c8ba902a63156
CacheStash <donationthing@gmail.com>
2026-02-25 19:35:43 +0700

    update 2026-02-25_19-35-05: fix spa routing and add image caching

--- pages/Policy.tsx
+++ pages/Policy.tsx
@@ -32,7 +32,7 @@ const Policy: React.FC = () => {
       <div className="grain-orb-base orb-top-right" />
       <div className="grain-orb-base orb-bottom-left" />
       <div className="grain-orb-base orb-top-right !top-auto !bottom-0 !-right-[10%] !bg-red-600/20" />
-      <div className="grain-orb-base orb-top-right !top-auto !bottom-[-15%] !left-[2%] !opacity-100 !scale-[1.8]" />
+      <div className="grain-orb-base orb-top-right !top-auto !bottom-[-15%] !left-[2%] !opacity-100 !scale-[1.8] !bg-red-600/40" />
       <div className="grain-orb-base orb-top-right !top-auto !bottom-[-15%] !left-[2%] !opacity-50 !scale-[2.8] blur-2xl" />
 
       <div className="max-w-full mx-auto relative z-10">
